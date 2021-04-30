f6825c1b7bc133e6f16efd931faf4d0bc8d2fb05
Kim-Seohee <suhee9305@gmail.com>
2021-04-30 17:41:41 +0900

    자바5일차: while문 예제

---
+++ java_ksh/src/day5/While1.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class While1 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 while문을 이용하여 출력하는 예제 */
+		int i = 1; // 초기화
+		while(i <= 5) { // 조건식
+			System.out.print(i + " "); // 실행문
+			i += 1; // (증감식의 의미)
+		}
+
+	}
+
+}
