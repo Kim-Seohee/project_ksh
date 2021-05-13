9ced77e81f0d714de38b1be44b8d133b08f134b1
Kim-Seohee <suhee9305@gmail.com>
2021-05-13 22:05:57 +0900

    자바13일차: Car 클래스를 통한 상속 예제

--- java_ksh/src/day13/Car.java
+++ java_ksh/src/day13/Car.java
@@ -1,18 +1,28 @@
 package day13;
 
 public class Car {
-	// 전원, 기어, 차종, 제조사
-	// 전원 on/off
+	//전원, 기어, 차종, 제조사 => 멤버변수로, 단, 접근제한자는 private
+	//전원 on/off
 	private boolean power;
-	// 기어 수동: 1, 2, 3, 4, 5, R 자동: P, D, N, R
-	private char gear;
+	//기어 수동 : 1,2,3,4,5,R 자동 : P, D, N, R
+	private char gear;  
 	private String type, company;
 	
+	public void turnOn()  {	power = true;}
+	public void turnOff() {	power = false;}
+	
+	public Car(String type, String company) {
+		this.type = type;
+		this.company = company;
+	}
+	public void print() {
+		System.out.println("제조 : " + company);
+		System.out.println("차종 : " + type);
+		System.out.println("전원 : " + power);
+		System.out.println("기어 : " + gear);
+	}
 	public boolean isPower() {
 		return power;
-	}
-	public void setPower(boolean power) {
-		this.power = power;
 	}
 	public char getGear() {
 		return gear;
@@ -29,9 +39,5 @@ public class Car {
 	public String getCompany() {
 		return company;
 	}
-	public void setCompany(String company) {
-		this.company = company;
-	}
-	
 	
 }
