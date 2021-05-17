299a7b2910b9ed401e84ae3fd10ea7fbff882a89
Kim-Seohee <suhee9305@gmail.com>
2021-05-17 17:30:26 +0900

    자바15일차: 인터페이스와 구현

---
+++ java_ksh/src/day15/RemoteControl.java
@@ -0,0 +1,13 @@
+package day15;
+
+public interface RemoteControl {
+	// 전원 켜기/끄기
+	public void powerOn();
+	public void powerOff();
+	/* 기능:
+	 * 매개변수:
+	 * 리턴타입:
+	 * 메소드명:
+	 * 정리만 하고 실제 구현은 하지 않아도 된다.
+	 * */
+}
