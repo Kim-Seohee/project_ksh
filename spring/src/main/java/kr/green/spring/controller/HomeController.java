a67bfcfeedb467bae5d0337e825ffb0cf008ec87
Kim-Seohee <suhee9305@gmail.com>
2021-07-06 13:06:25 +0900

    SPRING: 로그인 기능 구현(로그인 유지X)

--- spring/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring/src/main/java/kr/green/spring/controller/HomeController.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.green.spring.service.MemberService;
-import lombok.Data;
+import kr.green.spring.vo.MemberVO;
 
 @Controller
 public class HomeController {
@@ -19,25 +18,27 @@ public class HomeController {
 	public ModelAndView home(ModelAndView mv) {
 		mv.setViewName("home"); // home.jsp를 찾음
 		mv.addObject("name", "홍길동");
-		System.out.println(memberService.getMember("abc123"));
+//		System.out.println(memberService.getMember("abc123"));
 		return mv;
 	}
 	
 	@RequestMapping(value = "/signin", method = RequestMethod.GET)
-	public ModelAndView signinGet(ModelAndView mv, UserVo user, @RequestParam("hobby") String[] hobby) {
+	public ModelAndView signinGet(ModelAndView mv) {
 		mv.setViewName("signin");
-		System.out.println("id: " + user.getId());
-		System.out.println("pw: " + user.getPw());
-		for(String tmp : hobby) {
-			System.out.println("취미: " + tmp);			
+		return mv;
+	}
+	@RequestMapping(value = "/signin", method = RequestMethod.POST)
+	public ModelAndView signinPost(ModelAndView mv, MemberVO user) {
+		System.out.println(user);
+		// 서비스에게 아이디와 비밀번호를 전달하면, 해당 정보가 DB에 있으면 회원 정보를 없으면 null을 반환
+		MemberVO dbUser = memberService.signin(user);
+		if(dbUser != null) {			
+			// 작업이 다 끝난 후 URI가 /인 곳으로 넘어감
+			mv.setViewName("redirect:/");
+		}else {
+			mv.setViewName("redirect:/signin");
 		}
-
 		return mv;
 	}
 	
-}
-@Data
-class UserVo{
-	private String id;
-	private String pw;
 }
