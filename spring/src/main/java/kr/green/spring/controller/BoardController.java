d6fad064bf3e33092319863ebbb5f7f315284b91
Kim-Seohee <suhee9305@gmail.com>
2021-07-07 10:56:56 +0900

    SPRING: 게시글 상세 구현 완료

--- spring/src/main/java/kr/green/spring/controller/BoardController.java
+++ spring/src/main/java/kr/green/spring/controller/BoardController.java
@@ -29,4 +29,13 @@ public class BoardController {
 		mv.setViewName("board/list");
 		return mv;
 	}
+	@RequestMapping(value="/board/detail")
+	public ModelAndView boardDetail(ModelAndView mv, Integer num) { // int는 null값을 다루지 못하기 때문에 Integer를 쓴다.
+		// 서비스에게 번호를 주면서 게시글을 가져오라고 시킴
+		BoardVO board = boardService.getBoard(num);
+		// 가져온 게시글을 화면에 전달, 화면으로 보낼 이름은 board로
+		mv.addObject("board", board); // 괄호 안은 화면에서 쓸 이름, 오른쪽은 실제 데이터
+		mv.setViewName("board/detail");
+		return mv;
+	}
 }
